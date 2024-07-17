a819742a2971f4d1b0bc408e18b31e94f792035b
Eduardo Moreira (Moreira) <edmoreira043557@gmail.com>
2024-07-17 10:23:21 -0300

    Refatoração

--- back_end/solicitacoes/AlterarSolicitacoes.php
+++ back_end/solicitacoes/AlterarSolicitacoes.php
@@ -17,40 +17,28 @@ class AlterarSolicitacoes extends ExecutarQueryMysql
 		$dataProcedimento = AntiInjecaoMysql ::AntiInjecaoMysql($_POST['data']);
 		$horaProcedimento = AntiInjecaoMysql ::AntiInjecaoMysql($_POST['hora']);
 
-		$execucaoPacientes = $this -> ExecutarQueryMysql(
-			"SELECT * FROM teste_sitcon.pacientes WHERE nome LIKE '$nomePaciente' AND dataNasc LIKE '$dataNasc' AND cpf LIKE '$cpf' AND status LIKE 'ativo';"
-		);
-
+		$execucaoPacientes = $this -> ExecutarQueryMysql("SELECT * FROM teste_sitcon.pacientes WHERE nome LIKE '$nomePaciente' AND dataNasc LIKE '$dataNasc' AND cpf LIKE '$cpf' AND status LIKE 'ativo';");
 		$dadosPaciente = mysqli_fetch_assoc($execucaoPacientes);
 		if (empty($dadosPaciente)) {
 			$this -> mensagem('O paciente não está cadastrado ou está inativo!');
 			return false;
 		}
 
-		$execucaoProfissioanl = $this -> ExecutarQueryMysql(
-			"SELECT * FROM teste_sitcon.profissional WHERE id LIKE '$profissional' AND status LIKE 'ativo';"
-		);
-
+		$execucaoProfissioanl = $this -> ExecutarQueryMysql("SELECT * FROM teste_sitcon.profissional WHERE id LIKE '$profissional' AND status LIKE 'ativo';");
 		$dadosProfissional = mysqli_fetch_assoc($execucaoProfissioanl);
 		if (empty($dadosProfissional)) {
 			$this -> mensagem('O profissional não está cadastrado!');
 			return false;
 		}
 
-		$execucaoTipoSolicitacao = $this -> ExecutarQueryMysql(
-			"SELECT * FROM teste_sitcon.tiposolicitacao WHERE id LIKE '$tipoSolicitacao' AND status LIKE 'ativo';"
-		);
-
+		$execucaoTipoSolicitacao = $this -> ExecutarQueryMysql("SELECT * FROM teste_sitcon.tiposolicitacao WHERE id LIKE '$tipoSolicitacao' AND status LIKE 'ativo';");
 		$dadosTipoSolicitacao = mysqli_fetch_assoc($execucaoTipoSolicitacao);
 		if (empty($dadosTipoSolicitacao)) {
 			$this -> mensagem('Não existe este tipo de solicitação no sistema!');
 			return false;
 		}
 
-		$execucaoProcedimentos = $this -> ExecutarQueryMysql(
-			"SELECT * FROM teste_sitcon.procedimentos WHERE id LIKE '$procedimentos' AND status LIKE 'ativo';"
-		);
-
+		$execucaoProcedimentos = $this -> ExecutarQueryMysql("SELECT * FROM teste_sitcon.procedimentos WHERE id LIKE '$procedimentos' AND status LIKE 'ativo';");
 		$dadosProcedimentos = mysqli_fetch_assoc($execucaoProcedimentos);
 		if (empty($dadosProcedimentos)) {
 			$this -> mensagem('Não existe este procedimento no sistema!');
@@ -58,17 +46,13 @@ class AlterarSolicitacoes extends ExecutarQueryMysql
 		}
 
 		if ($dadosProcedimentos['tipoSolicitacao_id'] == $dadosTipoSolicitacao['id']) {
-			$execucaoProfissionalAtende = $this -> ExecutarQueryMysql(
-				"SELECT * FROM teste_sitcon.profissionalatende WHERE status LIKE 'ativo' AND profissional_id LIKE '" . $dadosProfissional['id'] . "' AND procedimento_id LIKE '" . $dadosProcedimentos['id'] . "';;"
-			);
+			$execucaoProfissionalAtende = $this -> ExecutarQueryMysql("SELECT * FROM teste_sitcon.profissionalatende WHERE status LIKE 'ativo' AND profissional_id LIKE '" . $dadosProfissional['id'] . "' AND procedimento_id LIKE '" . $dadosProcedimentos['id'] . "';;");
 			if (!$execucaoProfissionalAtende) {
 				$this -> mensagem('Este profissional não atende este procedimento!');
 				return false;
 			}
 
-			$selecionarSolicitacaoPaciente = $this -> ExecutarQueryMysql(
-				"SELECT * FROM teste_sitcon.solicitacoes WHERE id_paciente LIKE '" . $dadosPaciente['id'] . "';"
-			);
+			$selecionarSolicitacaoPaciente = $this -> ExecutarQueryMysql("SELECT * FROM teste_sitcon.solicitacoes WHERE id_paciente LIKE '" . $dadosPaciente['id'] . "';");
 			if (empty(mysqli_fetch_assoc($selecionarSolicitacaoPaciente))) {
 				$cadastrarSolicitacao = $this -> ExecutarQueryMysql(
 				"INSERT INTO teste_sitcon.solicitacoes VALUES (
